c845cd570008549a7ffeef598270525677c41bf3
mikhailnezhnikov-pixel <mikhail.nezhnikov@gmail.com>
2026-09-21 20:57:43 +0900

    hotfix: point HK loader to Businesses finalize core r28

--- hk/loader.js
+++ hk/loader.js
@@ -3,8 +3,8 @@
   const HOST='app.hamsterking.games';
   const CORE_URL='https://hk-license.89.125.1.71.sslip.io/panel.js';
   const LOCK_KEY='__HK_BOOKMARKLET_RUNTIME__';
-  const LOADER_REV='loader-20260921-r24';
-  const EXPECTED_CORE_REV='core-20260921-r27-businesses-runner-canon';
+  const LOADER_REV='loader-20260921-r25';
+  const EXPECTED_CORE_REV='core-20260921-r28-businesses-finalize';
   const TIMEOUTS=[3500,6000,12000,20000];
   const RETRY_DELAYS=[700,1800,4000];
   const runtime=window[LOCK_KEY]||{active:false,promise:null,events:[],startedAt:'',revision:LOADER_REV};
