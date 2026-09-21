f289c29dabf166beb60cb061c08d74b48cd29441
mikhailnezhnikov-pixel <mikhail.nezhnikov@gmail.com>
2026-09-21 15:18:30 +0900

    fix: align HK loader with userscript 1.17.16 core

--- hk/loader.js
+++ hk/loader.js
@@ -3,8 +3,8 @@
   const HOST='app.hamsterking.games';
   const CORE_URL='https://hk-license.89.125.1.71.sslip.io/panel.js';
   const LOCK_KEY='__HK_BOOKMARKLET_RUNTIME__';
-  const LOADER_REV='loader-20260921-r14';
-  const EXPECTED_CORE_REV='core-20260921-r17-buildings-ui-compact';
+  const LOADER_REV='loader-20260921-r15';
+  const EXPECTED_CORE_REV='core-20260921-r18-buildings-active-fix';
   const TIMEOUTS=[3500,6000,12000,20000];
   const RETRY_DELAYS=[700,1800,4000];
   const runtime=window[LOCK_KEY]||{active:false,promise:null,events:[],startedAt:'',revision:LOADER_REV};
